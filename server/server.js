8ae493971c30232e6a3bce3afbf293839b403d70
Govinda Awasthi <gxa1987@mavs.uta.edu>
2026-06-13 02:25:21 -0500

    Server setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+
+// MiddleWare
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+
+// Home Routes
+app.get('/', (req, res) => {
+  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">AI TOOLS API</h1>')
+})
+
+
+// Server Port:
+const PORT = process.env.PORT || 3001
+    
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`)
+})
